bec86c91ff7e2255c6e8cfeea641140b89668da7
AleksK <kibinaleks@gmail.com>
2023-04-11 17:56:04 +0300

    week4

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿Random rnd = new Random();
+int myRandNum;
+int randSum = 0;
+for (int i = 0; i < 20; i++)
+{
+    myRandNum = rnd.Next(0, 100); //0-10
+    randSum = randSum+ myRandNum;
+    Console.WriteLine($"My randoom numeber is {myRandNum}.");
+}
+Console.WriteLine($"My random number total is {randSum}");
---
+++ WhileLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿Random rnd= new Random();
+int cpuRand;
+bool loopActive = true;
+while (loopActive)
+{
+    cpuRand = rnd.Next(1, 4);
+    //Console.WriteLine($"cpu generated {cpuRand}");
+    Console.WriteLine("Guess the number (1-3)");
+    int userNumb = Int32.Parse(Console.ReadLine());
+    if (userNumb == cpuRand)
+    {
+        Console.WriteLine("Congratulations, you've won!");
+        //loopActive = false;
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Wrong, try again!");
+    }
+
+}
+Console.WriteLine("Have a nice day!");
